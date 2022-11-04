fa7cfe39b1a4faadefd264ce9c5ee82cbc03e112
Weichen <wliu30@stuy.edu>
2022-11-04 09:16:18 -0400

    added stuff

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,17 @@ AEIOU : Brian Chen, Weichen Liu, Vansh Saboo
 11/02/22
 '''
 
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return()
+    render_template(homepage.html)
+
+
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
